6710115839d53c7f0dc4a8c9f6c7e8ddb28a4a63
maitrishah101 <maitri.shah101@gmail.com>
2018-01-16 09:36:57 -0800

    Reverse String

---
+++ leetcode/Reverse String.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String reverseString(String s) {
+        char[] word = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i < j){
+            char temp = word[i];
+            word[i] = word[j];
+            word[j] = temp;
+            i ++;
+            j --;
+        }
+        return new String(word);
+    }
+}
